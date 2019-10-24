24f91ed9f752064739cb03053f087acd6225a610
vagelisnaz <vagelisnazaris@gmail.com>
2019-10-24 14:43:34 +0300

    +

--- relevant_traversals_hypercube/relevant_traversals_hypercube.hpp
+++ relevant_traversals_hypercube/relevant_traversals_hypercube.hpp
@@ -1,5 +1,5 @@
-#ifndef REL_TRAV_H
-#define REL_TRAV_H
+#ifndef REL_TRAV_HYPER_H
+#define REL_TRAV_HYPER_H
 
 #include <list>
 #include <vector>
@@ -9,25 +9,28 @@
 #include "../curve_grid_hypercube/curve_grid_hypercube.hpp"
 
 
-class Relevant_Traversals {
+class Relevant_Traversals_hypercube {
 public:
-	Relevant_Traversals(int i, int j, int L, int hash_table_dimension, int w, int k);
-	~Relevant_Traversals();
+	Relevant_Traversals_hypercube(int i, int j, int table_size_hypercube, int K_matrix, int w, int k,int m,int M);
+	~Relevant_Traversals_hypercube();
 
-	void insert(Curve *curve, int hash_table_dimension, int w,
-		int k, int bits_of_each_hash, int M, vector<unsigned>& m_powers,
+	void insert(Curve *curve, int table_size_hypercube, int w,
+		int k, int bits_of_each_hash, int M,
 		float **G_matrix, int K_matrix, int curve_dimensinion);
 	void print_hash_tables();
 
 	list<vector<Tuple*>*>& get_relevant_traversals() {return relevant_traversals;}
-	vector<Hash_Table_Hypercube*>& get_hash_tables() {return hash_tables_R;}
-
+	vector<Hash_Table_Hypercube*>& get_hash_tables() {return hash_tables;}
+	vector<vector<unsigned>*>& get_m_powers_array() {return m_powers_array;}
+	int get_num_of_traversals() {return relevant_traversals.size();}
 
 private:
     list<vector<Tuple*>*> relevant_traversals;
-	vector<Hash_Table_Hypercube*>  hash_tables_R;
-    //vector<Hash_Table*> hash_tables;
+	//vector<Hash_Table_Hypercube*>  hash_tables_R;
+    vector<Hash_Table_Hypercube*> hash_tables;
+	vector<vector<unsigned>*> m_powers_array;
 
+	int table_size_hypercube;
 	int length_i;
 	int length_j;
 };
